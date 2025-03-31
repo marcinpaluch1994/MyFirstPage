8eacb8aa06b7106ce70f52636c8f4ce6adf91434
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2025-03-31 22:10:16 +0200

    Update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.getElementById('get-data').addEventListener('click', async () => {
     try {
-        const response = await fetch('https://4557-194-230-148-34.ngrok-free.app/random-poem');
+        const response = await fetch('https://b4c0-194-230-148-34.ngrok-free.app/random-poem');
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
